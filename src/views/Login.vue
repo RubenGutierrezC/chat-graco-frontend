4698c01b7dfea3771324963ccc3b14e4df161618
Ruben Gutierrez <rubdeivis@gmail.com>
2021-08-04 21:51:54 -0400

    advances

---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card-container">
+    <div class="card card-form mt-5">
+      <div class="card-header">
+        Iniciar sesión
+      </div>
+      <div class="card-body">
+        <input
+          class="form-control mb-3"
+          type="text"
+          placeholder="username"
+          aria-label="default input example"
+        />
+        <button class="btn btn-primary">Iniciar sesión</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    username: "",
+  }),
+  methods: {
+    login() {
+      this.$socket.emit("join", { username: this.username }, (resp) => {
+        console.log(resp);
+        if (resp) {
+          this.$store.dispatch("user", this.username);
+          this.$router.push("/chat");
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-container {
+  display: flex;
+  justify-content: center;
+}
+
+.card-form {
+  width: 100%;
+  max-width: 400px;
+}
+</style>
